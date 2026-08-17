199763a0a50c470cd1553c173e9459e8585a65cb
Elad <elad291297@gmail.com>
2026-08-17 11:39:11 +0300

    Increase EKS capacity for Jenkins workloads

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -131,9 +131,9 @@ resource "aws_eks_node_group" "main" {
   instance_types = ["t3.small"]
 
   scaling_config {
-    desired_size = 3
+    desired_size = 4
     min_size     = 3
-    max_size     = 3
+    max_size     = 4
   }
 
   depends_on = [
